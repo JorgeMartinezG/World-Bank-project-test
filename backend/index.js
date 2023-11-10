988da7a038c2c814a84b7dcbe6a3fb79af0dd278
Jorge Martinez <jorgemg0327@gmail.com>
2023-11-10 08:39:37 +0100

    Use server capabilities

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,61 @@
+const findTagsByPath = require("xml-utils/find-tags-by-path.js");
+const findTagByName = require("xml-utils/find-tag-by-name.js");
 const express = require("express");
-var cors = require('cors')
+var cors = require("cors");
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.get("/", (request, response) => {
-    const layers = [
-        {
-            title: "GPWv4: Population Density – 2015",
-            url: "http://sedac.ciesin.columbia.edu/geoserver/wms",
-            id: "gpw-v4:gpw-v4-population-density_2015"
-        },
-        {
-            title: "Probabilities of Urban Expansion to 2030",
-            url: "http://sedac.ciesin.columbia.edu/geoserver/wms",
-            id: "lulc:lulc-global-grid-prob-urban-expansion-2030"
-        }
-    ]
+  // Check layers.txt file for more layer ids.
+  const allowedLayers = [
+    "gpw-v4:gpw-v4-population-density_2015",
+    "lulc:lulc-global-grid-prob-urban-expansion-2030",
+    "wildareas-v2:wildareas-v2-last-of-the-wild-geographic",
+  ];
 
-    response.json({total: layers.length, layers});
+  const serverUrl = "http://sedac.ciesin.columbia.edu/geoserver/wms";
+
+  const capabilitiesUrl = `${serverUrl}?service=WMS&request=getcapabilities`;
+
+  fetch(capabilitiesUrl)
+    .then((res) => res.text())
+    .then((xmlstr) => {
+      const xmlArray = findTagsByPath(xmlstr, ["Layer", "Layer"]).map(
+        (t) => t.outer,
+      );
+      const parsedLayers = xmlArray.map((xmlItem) => ({
+        title: findTagByName(xmlItem, "Title").inner,
+        id: findTagByName(xmlItem, "Name").inner,
+      }));
+
+      return parsedLayers;
+    })
+    .then((layers) =>
+      layers.filter((layer) => allowedLayers.includes(layer.id)),
+    )
+    .then((layers) => layers.map((layer) => ({ ...layer, url: serverUrl })))
+    .then((layers) => response.json({ total: layers.length, layers }));
+
+  // For using hardcoded layers, uncomment the following block.
+  /*
+  const layers = [
+    {
+      title: "GPWv4: Population Density – 2015",
+      url: "http://sedac.ciesin.columbia.edu/geoserver/wms",
+      id: "gpw-v4:gpw-v4-population-density_2015",
+    },
+    {
+      title: "Probabilities of Urban Expansion to 2030",
+      url: "http://sedac.ciesin.columbia.edu/geoserver/wms",
+      id: "lulc:lulc-global-grid-prob-urban-expansion-2030",
+    },
+  ];
+
+  response.json({ total: layers.length, layers });
+  */
 });
 
 app.listen(3000, () => {
-    console.log("Listen on the port 3000...");
+  console.log("Listen on the port 3000...");
 });
